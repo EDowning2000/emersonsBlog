eaa69fb79f92df6198676b14482cefe06cd95915
Emerson Downing <emersondowning@gmail.com>
2020-12-16 12:36:31 -0500

    got my global styles kinda setup

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,14 +1,16 @@
 import React from "react";
-import styles from './Nav.module.css'
+import styles from "./Nav.module.css";
 
 const Nav = () => {
   return (
-   <div className={styles.container}>
-     <h5>My Logo</h5>
-     <h5>About Me</h5>
-     <h5>Contact</h5>
-     <h5>My Work</h5>
-   </div>
+    <div className={styles.container}>
+      <h6 className={styles.logo}>My Logo</h6>
+      <div className={styles.links}>
+        <h6 className={styles.single}>About Me</h6>
+        <h6 className={styles.single}>Contact</h6>
+        <h6 className={styles.single}>My Work</h6>
+      </div>
+    </div>
   );
 };
 export default Nav;
